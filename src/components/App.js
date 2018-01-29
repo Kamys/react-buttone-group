28a68f9552940afd089a93ba3b034f632c58ff8a
Kamys <nikita.kamyshenko@mail.ru>
2018-01-30 00:30:37 +0300

    Add Route

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,69 @@
 import React, {Component} from 'react';
 import ButtonGroup from './ButtonGroup';
+import {
+    BrowserRouter,
+    Route,
+    Link,
+    Switch
+} from 'react-router-dom'
+
 
 export default class App extends Component {
     render() {
-        let dataBtn = [];
+        function getFaculty() {
+            return [
+                {name: "Второго высшего образования и магистерской подготовки", key: 10004},
+                {name: "Заочного обучения", key: 9},
+                {name: "Общественного питания и товароведения", key: 10},
+                {name: "Отдел аспирантуры", key: 12},
+                {name: "Подготовительный факультет", key: 14},
+                {name: "СПО - заочная форма", key: 10045},
+                {name: "Среднего профессионального образования", key: 1},
+                {name: "Среднего профессионального образования №1", key: 10083},
+                {name: "Среднего профессионального образования №2", key: 10084},
+                {name: "Таможенного дела и информационных технологий", key: 7},
+                {name: "Учебно-методический центр", key: 10046},
+                {name: "Учебно-методический центр повышения квалификации и переподготовки кадров", key: 15},
+                {name: "Экономики и менеджмента", key: 11},
+                {name: "Юридический", key: 8}];
+        }
 
-        dataBtn.push({name: "Второго высшего образования и магистерской подготовки", key: 10004});
-        dataBtn.push({name: "Заочного обучения", key: 9});
-        dataBtn.push({name: "Общественного питания и товароведения", key: 10});
-        dataBtn.push({name: "Отдел аспирантуры", key: 12});
-        dataBtn.push({name: "Подготовительный факультет", key: 14});
-        dataBtn.push({name: "СПО - заочная форма", key: 10045});
-        dataBtn.push({name: "Среднего профессионального образования", key: 1});
-        dataBtn.push({name: "Среднего профессионального образования №1", key: 10083});
-        dataBtn.push({name: "Среднего профессионального образования №2", key: 10084});
-        dataBtn.push({name: "Таможенного дела и информационных технологий", key: 7});
-        dataBtn.push({name: "Учебно-методический центр", key: 10046});
-        dataBtn.push({name: "Учебно-методический центр повышения квалификации и переподготовки кадров", key: 15});
-        dataBtn.push({name: "Экономики и менеджмента", key: 11});
-        dataBtn.push({name: "Юридический", key: 8});
-        return (<ButtonGroup data={dataBtn} onclick={function (key) {
-            console.log("btn key = " + key);
-        }}/>);
+        function getSpeciality() {
+            return [
+                {name: "Информационные системы (по отраслям)", key: 1370},
+                {name: "Организация и технология защиты информации", key: 1411},
+                {name: "Право и организация социального обеспечения", key: 1394},
+                {name: "Прикладная информатика (по отраслям)", key: 1363},
+                {name: "Технология продукции общественного питания", key: 1423},
+                {name: "Товароведение и экспертиза качества потребительских товаров", key: 1434}
+            ];
+        }
+
+        function selectSpeciality(id) {
+            this.props.history.push('/speciality/:' + id);
+        }
+
+        return (
+            <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/"
+                               render={(props) => (<ButtonGroup data={getFaculty()} onclick={selectSpeciality}/>)}/>
+                        <Route path={`speciality/:id`} component={ButtonGroup}/>
+                    </Switch>
+            </BrowserRouter>
+        );
     }
+}
+
+{/*
+<Router>
+    <div>
+        <Route exact path="/"
+               render={(props) => (<ButtonGroup data={getFaculty()} onclick={selectSpeciality}/>)}/>
+        <Route exact path="/speciality:id"
+               render={(props) => (<ButtonGroup data={getSpeciality(props.route.id)}
+                                                onclick={selectSpeciality}/>)}/>
+    </div>
+</Router>
+*/
 }
